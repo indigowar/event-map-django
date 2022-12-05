68930b7b02ff94ea6c1d20207653c7a06bad1ed8
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-12-06 01:47:46 +0300

    add a code for model Competitor

--- events/models.py
+++ events/models.py
@@ -43,6 +43,7 @@ class CoFoundingRange(models.Model):
 
 class Competitor(models.Model):
     name = models.CharField(max_length=255)
+    code = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
