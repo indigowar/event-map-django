3a1dd366d4e21540457b28b278e90253bf4cc11d
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-12-06 03:30:14 +0300

    add setting for choosing debug mode

--- eventmap/settings.py
+++ eventmap/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-y7y$3*)2&*%x8ljtrapcs$!ibte5rwekhopbu+$k%c7yotka^p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('ENV') == 'dev'
 
 ALLOWED_HOSTS = []
 
