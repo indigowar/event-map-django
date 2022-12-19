da82cb8581952c2094a4b62e9384b905f69f4698
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2022-12-19 15:44:59 +0300

    Absolutely great fix of this slash, is django insane?

--- eventmap/urls.py
+++ eventmap/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('api/v1/', include('events.urls')),
     path('api-auth/', include('rest_framework.urls')),
 
-    path('api/token', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
     path("admin/", admin.site.urls),
 ]
