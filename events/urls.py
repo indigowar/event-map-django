f7a1e7565cd5b50d9c2eb5210aea17fd2a92c009
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2023-01-04 18:19:48 +0300

    Replace method on all update methods

--- events/urls.py
+++ events/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     })),
     path('organizer/<int:pk>', views.OrganizerViewSet.as_view({
         'get': 'retrieve',
-        'post': 'update',
+        'put': 'update',
         'delete': 'destroy',
     })),
 
@@ -20,7 +20,7 @@ urlpatterns = [
 
     path('organizer_level/<int:pk>', views.OrganizerLevelViewSet.as_view({
         'get': 'retrieve',
-        'post': 'update',
+        'put': 'update',
         'delete': 'destroy',
     })),
 
@@ -30,7 +30,7 @@ urlpatterns = [
     })),
     path('competitor/<int:pk>', views.CompetitorViewSet.as_view({
         'get': 'retrieve',
-        'post': 'update',
+        'put': 'update',
         'delete': 'destroy',
     })),
 
@@ -40,7 +40,7 @@ urlpatterns = [
     })),
     path('founding_type/<int:pk>', views.FoundingTypeViewSet.as_view({
         'get': 'retrieve',
-        'post': 'update',
+        'put': 'update',
         'delete': 'destroy',
     })),
 
