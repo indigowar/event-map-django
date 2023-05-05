5a6ede7e36b50d1d964445b893fed3eb8626bf1f
Indigowar <indigowar.github@gmail.com>
2023-05-05 22:48:08 +0300

    added all start scripts into  entrypoint in bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 python manage.py migrate
-#./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
+
+if [ "$ENV" = "dev" ]; then
+  ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('dev_admin', 'admin@example.com', 'adminpass')"
+else
+  ./manage.py shell -c "from django.contrib.auth.models import User; User.get(username='dev_admin').delete()"
+fi
 
 python manage.py runserver 0.0.0.0:10000
--- management/commands/initgroups.py
+++
@@ -1,8 +0,0 @@
-from django.core.management import BaseCommand
-from django.contrib.auth.models import Group, Permission, User
-
-import logging
-
-# GROUPS = {
-#     ""
-# }
