ca1faf172a60c777e01ab54b34fa294c21e92f84
Indigowar <indigowar.github@gmail.com>
2023-05-07 14:30:52 +0300

    fix the typo in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ python manage.py migrate
 if [ "$ENV" = "dev" ]; then
   ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('dev_admin', 'admin@example.com', 'adminpass')"
 else
-  ./manage.py shell -c "from django.contrib.auth.models import User; User.get(username='dev_admin').delete()"
+  ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.get(username='dev_admin').delete()"
 fi
 
 python manage.py runserver 0.0.0.0:10000
